de1a8a4add86a0f96bc756306d342cf5d437eba0
Ajaz203 <ajazahmad203@gmail.com>
2024-02-14 21:22:56 +0000

    commit on 14-02

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -21,6 +21,24 @@ sap.ui.define([
                 oRouter.navTo("details",{
                     CustomerID: oContext.CustomerID
                 });
-            }
+            },
+            onSearch: function(oEvent) {
+                // Get the search query entered by the user
+                var sQuery = oEvent.getParameter("query").toLowerCase(); // Convert to lowercase for case-insensitive search
+            console.log(sQuery)
+                // Get the reference to the table
+                var oTable = this.byId("idsmartTable");
+            
+                // Get the binding context of the table
+                var oBinding = oTable.getBinding("items");
+            
+                // Apply the filter to the binding
+                if (sQuery) {
+                    var oFilter = new sap.ui.model.Filter("CustomerID", sap.ui.model.FilterOperator.Contains, sQuery);
+                    oBinding.filter([oFilter]);
+                } else {
+                    oBinding.filter([]);
+                }
+            },
         });
     });
